9a89805ce28c0b56ab47c68a12b8308176edb41e
JohannaBerthelsen <johannalberthelsen@gmail.com>
2025-10-12 09:35:12 +0200

    Observables

--- apm-begin/src/app/products/product.service.ts
+++ apm-begin/src/app/products/product.service.ts
@@ -1,7 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { catchError, Observable, tap, of, throwError } from 'rxjs';
 import { Product } from './product';
+import { ProductData } from './product-data';
+import { HttpErrorService } from '../utilities/http-error.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,20 +12,29 @@ export class ProductService {
   private productsUrl = 'api/products';
 
   private http = inject(HttpClient);
+  private errorService = inject(HttpErrorService);
 
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.productsUrl)
       .pipe(
-        tap(() => console.log('In http.get pipeline'))
-      )
+        tap(() => console.log('In http.get pipeline')),
+        catchError(err => this.handleError(err))
+      );
   }
 
   getProduct(id: number): Observable<Product> {
     const productUrl = this.productsUrl + '/' + id;
     return this.http.get<Product>(productUrl)
       .pipe(
-        tap(() => console.log('in the single http.get pipeline'))
+        tap(() => console.log('in the single http.get pipeline')),
+        catchError(err => this.handleError(err))
       );
+  }
+
+  private handleError(err: HttpErrorResponse): Observable<never>{
+    const formattedMessage = this.errorService.formatError(err);
+    return throwError(() => formattedMessage);
+
   }
 
 
